a549a529a009aa5007e7d16ab14ffcaf17e52be5
spiuri <61064112+spiuri@users.noreply.github.com>
2022-08-19 21:51:46 -0300

    Aula2 Completa

---
+++ imagens.js
@@ -0,0 +1,16 @@
+
+//codigo das imagens
+
+let imagemEstrada;
+let imagemAtor;
+let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
+
+function preload(){
+    imagemEstrada = loadImage("material/estrada.png");
+    imagemAtor = loadImage("material/ator-1.png");
+    imagemCarro = loadImage("material/carro-1.png");
+    imagemCarro2 = loadImage("material/carro-2.png");
+    imagemCarro3 = loadImage("material/carro-3.png");
+}
--- sketch.js
+++ sketch.js
@@ -1,20 +1,13 @@
-let imagemEstrada;
-let imagemAtor;
-let imagemCarro;
+
 
 // carro
 
-let xCarro=600;
+
 
 //ator
 
-let yAtor = 366;
 
-function preload(){
-    imagemEstrada = loadImage("material/estrada.png");
-    imagemAtor = loadImage("material/ator-1.png");
-    imagemCarro = loadImage("material/carro-1.png");
-}
+
 
 function setup() {
     createCanvas(500, 400);
@@ -26,27 +19,12 @@ function setup() {
     mostraCarro();
     movimentaCarro();
     movimentaAtor();
+    voltaPocisaoInicialdoCarro();
   }
 
-  function mostraAtor(){
-    image(imagemAtor,100,yAtor,30,30);
-  }
+ 
 
 
-  function mostraCarro(){
-    image(imagemCarro,xCarro,40,50,40);
-  }
-
-  function movimentaCarro(){
-    xCarro -=2;
-  }
-
-  function movimentaAtor(){
-    if(keyIsDown(UP_ARROW)){
-        yAtor -=3;
-    }
-    if(keyIsDown(DOWN_ARROW)){
-        yAtor +=3;
-    }
-  }
+ 
+  
   
