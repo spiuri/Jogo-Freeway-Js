574f91473fbff530088a570841bf1e5e3a8f4f48
spiuri <61064112+spiuri@users.noreply.github.com>
2022-08-19 21:30:21 -0300

    aula 1 completa

--- sketch.js
+++ sketch.js
@@ -1,8 +1,52 @@
+let imagemEstrada;
+let imagemAtor;
+let imagemCarro;
+
+// carro
+
+let xCarro=600;
+
+//ator
+
+let yAtor = 366;
+
+function preload(){
+    imagemEstrada = loadImage("material/estrada.png");
+    imagemAtor = loadImage("material/ator-1.png");
+    imagemCarro = loadImage("material/carro-1.png");
+}
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(500, 400);
   }
   
   function draw() {
-    background(220);
+    background(imagemEstrada);
+    mostraAtor();
+    mostraCarro();
+    movimentaCarro();
+    movimentaAtor();
+  }
+
+  function mostraAtor(){
+    image(imagemAtor,100,yAtor,30,30);
+  }
+
+
+  function mostraCarro(){
+    image(imagemCarro,xCarro,40,50,40);
+  }
+
+  function movimentaCarro(){
+    xCarro -=2;
+  }
+
+  function movimentaAtor(){
+    if(keyIsDown(UP_ARROW)){
+        yAtor -=3;
+    }
+    if(keyIsDown(DOWN_ARROW)){
+        yAtor +=3;
+    }
   }
   
